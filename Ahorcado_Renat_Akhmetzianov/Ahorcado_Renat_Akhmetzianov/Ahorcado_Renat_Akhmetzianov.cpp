bcaa8dfb3dc0b09117c165812582898a49177753
by-gedamire <renata@vedrunavall.cat>
2024-10-23 19:23:22 +0200

    Started a map
    Started the creation of a map.

--- Ahorcado_Renat_Akhmetzianov/Ahorcado_Renat_Akhmetzianov/Ahorcado_Renat_Akhmetzianov.cpp
+++ Ahorcado_Renat_Akhmetzianov/Ahorcado_Renat_Akhmetzianov/Ahorcado_Renat_Akhmetzianov.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#define SIZE 10
+
+void printMap(char map[SIZE][SIZE], int playerX, int playerY) {
+    system("cls");  
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (i == playerX && j == playerY) {
+                printf("X ");
+            }
+            else {
+                printf("%c ", map[i][j]);
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
+
